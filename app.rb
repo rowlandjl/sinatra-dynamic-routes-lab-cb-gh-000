34a188bfee1b55b5fff00e1a3902ceb59e03b6a9
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-05 13:07:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
+  end
+
+  get '/square/:number' do
+  end
+
+  get '/say/:number' do
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  end
+
+  get '/:operation/:number1/:number2' do
   end 
 
 end
