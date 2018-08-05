f36cc14838662b51d17a3fb506fc0bfeeedf8fb1
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-05 13:25:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,6 +20,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    "#{@w1 }" + "#{@w2 }" + "#{@w3 }" + "#{@w4 }" + "#{@w5}."
   end
 
   get '/:operation/:number1/:number2' do
@@ -35,7 +41,7 @@ class App < Sinatra::Base
       "#{@num1.to_i / @num2.to_i}"
     elsif @operation == 'multiply'
       "#{@num1.to_i * @num2.to_i}"
-    end 
+    end
   end
 
 end
