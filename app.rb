184793c0cc9265c4b3449e65d38487966e152b5a
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-05 13:08:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
   end
 
   get '/square/:number' do
