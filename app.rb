66101c38673a6af80af2a05922c6a5b44fc57956
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-05 13:22:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,8 +26,16 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @num1 = params[:number1]
     @num2 = params[:number2]
-    "#{@num1.to_i + @num2.to_i}"
-    "#{@num1.to_i * @num2.to_i}"
+
+    if @operation == 'add'
+      "#{@num1.to_i + @num2.to_i}"
+    elsif @operation == 'subtract'
+      "#{@num2.to_i - @num1.to_i}"
+    elsif @operation == 'divide'
+      "#{@num1.to_i / @num2.to_i}"
+    elsif @operation == 'multiply'
+      "#{@num1.to_i * @num2.to_i}"
+    end 
   end
 
 end
