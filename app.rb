816b8f5e71bbc1b520c7f03be3154cbdb455b280
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-05 13:10:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
